ea1cb2ae0ec43ebccfd4dde02356d3dc1e02e02c
George <lunohod0002@gmail.com>
2025-11-09 22:55:02 +0300

    feat: add redis - Add RedisConfig - Add cashing all controllers with default 1 day ttl time

--- src/main/java/com/example/backend_vkr/controllers/StationController.java
+++ src/main/java/com/example/backend_vkr/controllers/StationController.java
@@ -7,10 +7,12 @@ import com.example.backend_vkr.models.PagedResponse;
 import com.example.backend_vkr.models.StationResponse;
 import com.example.backend_vkr.services.AttractionService;
 import com.example.backend_vkr.services.StationService;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@EnableCaching
 @RestController
 public class StationController implements StationApi {
 
@@ -24,19 +26,19 @@ public class StationController implements StationApi {
     }
 
 
-
+    @Cacheable(value = "station")
     @Override
     public StationResponse getStationInfo(String stationName, String branch) {
         return stationService.getStationByNameAndBranch(stationName,branch);
     }
-
+    @Cacheable(value = "stationAttractions")
     @Override
     public PagedResponse<AttractionResponse> getStationAttractions(Long id, int page, int size) {
         return attractionService.getStationAttractions(id,page,size);
 
     }
 
-
+    @Cacheable(value = "attraction")
     @Override
     public AttractionInfoResponse getAttraction(Long id) {
         return attractionService.findAttractionById(id);
